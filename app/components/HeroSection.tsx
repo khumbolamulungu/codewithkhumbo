50726f40e510e3732fb5de7ece9837256b39cd88
Khumbo Klein Chilamwa <68913833+khumbolamulungu@users.noreply.github.com>
2025-08-23 19:05:41 +0200

    hero section updated text animation

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -7,7 +7,25 @@ import { motion } from "framer-motion";
 import { fadeInUp, scaleIn } from "@/utils/animations";
 
 
+const sentence = {
+  hidden: { opacity: 1 },
+  visible: {
+    opacity: 1,
+    transition: {
+      delay: 0.3,
+      staggerChildren: 0.05, // delay between each letter
+    },
+  },
+};
+
+const letter = {
+  hidden: { opacity: 0, y: 20 },
+  visible: { opacity: 1, y: 0 },
+};
+
+
 export default function HeroSection(){
+    const text = "Hi, I'm Khumbo Klein Chilamwa";
     return(
         <section className="py-28 container max-w-7xl mx-auto px-4">
             <div className="max-w-3xl mx-auto text-center">
@@ -21,7 +39,31 @@ export default function HeroSection(){
 
 
                 {/* h1 text */}
-                <motion.h1 {...fadeInUp} transition={{delay: 0.3}} className="text-4xl md:text-6xl font-bold mb-6">Hi, I&apos;m <span className="text-primary">Khumbo Klen Chilamwa</span></motion.h1>
+                {/* <motion.h1 {...fadeInUp} transition={{delay: 0.3}} className="text-4xl md:text-6xl font-bold mb-6">Hi, I&apos;m <span className="text-primary">Khumbo Klen Chilamwa</span></motion.h1> */}
+
+                <motion.h1
+                    variants={sentence}
+                    initial="hidden"
+                    animate="visible"
+                    transition={{delay: 0.9}}
+                    className="text-4xl md:text-6xl font-bold mb-6"
+                    >
+                    {/* Intro text */}
+                    {"Hi, I'm ".split("").map((char, i) => (
+                        <motion.span key={i} variants={letter}>
+                        {char}
+                        </motion.span>
+                    ))}
+
+                    {/* Highlighted name */}
+                    <span className="text-primary">
+                        {"Khumbo Klein Chilamwa".split("").map((char, i) => (
+                        <motion.span key={`name-${i}`} variants={letter}>
+                            {char}
+                        </motion.span>
+                        ))}
+                    </span>
+                </motion.h1>
 
                 <motion.p {...fadeInUp} transition={{delay: 0.5}} className="text-xl md:text-2xl text-gray-600 dark:text-gray-300 mb-8">Full-Stack Developer | UI/UX Specialist | Content Writing Enthusiast</motion.p>
 
